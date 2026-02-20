e14e5a9f8fc94ced5c0660ba73c00b8638951cd6
Dipin101 <khatridipin10@gmail.com>
2026-02-20 02:42:39 -0500

    checking json

--- server/controller/firebase/firebaseAdmin.js
+++ server/controller/firebase/firebaseAdmin.js
@@ -1,11 +1,7 @@
 require("dotenv").config({ path: __dirname + "/../../config/.env" }); // load .env
 const admin = require("firebase-admin");
 
-const serviceAccountPath = process.env.FIRE_BASE_CREDENTIAL;
-
-if (!serviceAccountPath) {
-  throw new Error("FIRE_BASE_CREDENTIAL is not defined! Check your .env file.");
-}
+const serviceAccountPath = JSON.parse(process.env.FIRE_BASE_CREDENTIAL);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccountPath),
