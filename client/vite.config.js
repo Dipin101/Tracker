9d4fe3c2f3b075af2fca0e4caae07c856e8e3d7d
Dipin101 <khatridipin10@gmail.com>
2026-02-19 23:28:47 -0500

    Fixing Vercel build: add Firebase alias for Vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  optimizeDeps: {
-    include: ["firebase/app", "firebase/auth"],
+  resolve: {
+    alias: {
+      // Make Vite resolve firebase properly
+      "firebase/app": path.resolve(
+        "./node_modules/firebase/app/dist/index.esm.js",
+      ),
+      "firebase/auth": path.resolve(
+        "./node_modules/firebase/auth/dist/index.esm.js",
+      ),
+    },
   },
 });
